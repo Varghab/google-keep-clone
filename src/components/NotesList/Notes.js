f5585b6da49309f430f3787b1a2368bc7a771023
Varghab <59419650+Varghab@users.noreply.github.com>
2023-02-25 22:32:23 +0530

    Update Notes.js

--- src/components/NotesList/Notes.js
+++ src/components/NotesList/Notes.js
@@ -1,4 +1,4 @@
-import React, { useState, useContext } from 'react';
+import React, { useContext } from 'react';
 import classes from './Notes.module.css';
 import NoteContext from '../../store/note-context';
 
